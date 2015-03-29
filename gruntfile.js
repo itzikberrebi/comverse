47f5eb077e37b79b59ed79a9b27cd68f665af4f0
itzikberrebi <itzikb800@gmail.com>
2015-03-29 15:56:19 +0300

    md2html files

--- gruntfile.js
+++ gruntfile.js
@@ -11,11 +11,23 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+
+    md2html: {
+      one_file: {
+        options: {},
+        files: [{
+          src: ['index.md'],
+          dest: 'outline.html'
+        }]
+      }
     }
+
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-md2html');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
